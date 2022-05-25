2880303e56ce4268abc933c21c2dad86fbf5c312
Rafael-Cirino <r223730@dac.unicamp.br>
2022-05-25 08:52:14 -0300

    Tratando informações versão 1

--- main.py
+++ main.py
@@ -1,38 +1,48 @@
-arq = open("Data/bus_9.txt")
+from numpy import NaN
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+PATH_RAW = os.getenv("PATH_RAW")
+PATH_JSON = os.getenv("PATH_JSON")
+
+arq = open(PATH_RAW + "bus_9.txt")
 linhas = arq.readlines()
 
 list_data = []
+len_cols = len(linhas[0])
 for linha in linhas:
-    #linha = linha.replace("\t", "\t;")
+    # linha = linha.replace("\t", "\t;")
     linha = linha.split("\t")
     if "\n" in linha:
         linha.remove("\n")
+
+    for j in range(len(linha), len_cols):
+        linha.append("")
+
     list_data.append(linha)
 
-print(list_data[0], list_data[1])
+# print(list_data[0], list_data[1])
 print(len(list_data[0]), len(list_data[1]))
 
-#Montar dicionário
+# Montar dicionário
 dict_data = {}
 
 for i, col in enumerate(list_data[0]):
     list_aux = []
     for linha in list_data[1:]:
-        if (len(linha)-1) > i:
-            list_aux.append(linha[i])
-            #print(i, len(linha))
-    
+        # if (len(linha)-1) > i:
+        list_aux.append(linha[i])
+        # print(i, len(linha))
+
     dict_data[col] = [list_aux]
-    print(col)
 
 print(dict_data["Timestamp"])
 print(dict_data["NTech2"])
 
-import csv
+import json
 
-with open('Data/bus_9.txt','r') as csvfile:
-    plots = csv.reader(csvfile, delimiter = '\t')
-
-    for row in plots:
-        print(row, len(row))
-
+with open(PATH_JSON + "data.json", "w") as j_file:
+    json.dump(dict_data, j_file)
